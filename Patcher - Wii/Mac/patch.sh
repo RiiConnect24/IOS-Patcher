58376178dcdbe8197ccfdc027ef5c42626e3bc69
larsenv <larsen@weborican.com>
2016-12-03 09:30:36 -0600

    Fixed patching IOS31

--- Patcher - Wii/Mac/patch.sh
+++ Patcher - Wii/Mac/patch.sh
@@ -3,18 +3,16 @@ echo "This is only for the Wii, please don't install this on a Wii U."
 echo "If you want to patch the IOS for Wii U, please download the other version."
 echo "Please install Mono or else this won't work."
 printf '\e[0;31m%s\e[0m \n' '1. Downloading IOS from NUS...'
-mono Sharpii.exe NUSD -ios 31 -v latest -o IOS31
-mono Sharpii.exe NUSD -ios 80 -v latest -o IOS80
+mono Sharpii.exe NUSD -ios 31 -v latest -o IOS31-old
+mono Sharpii.exe NUSD -ios 80 -v latest -o IOS80-old
 printf '\e[0;31m%s\e[0m \n' '2. Unpacking IOS...'
-cp IOS31/000000010000001fv3608.wad IOS31-old.wad
-cp IOS80/0000000100000050v6944.wad IOS80-old.wad
+cp IOS31-old/000000010000001fv3608.wad IOS31-old.wad
+cp IOS80-old/0000000100000050v6944.wad IOS80-old.wad
 mono Sharpii.exe WAD -u IOS31-old.wad IOS31/
 mono Sharpii.exe WAD -u IOS80-old.wad IOS80/
 printf '\e[0;31m%s\e[0m \n' '3. Patching the APP file...'
-./xdelta3 -f -d -s ./IOS31/00000006.app 00000006-31.delta 00000006.app
-mv 00000006.app IOS31
-./xdelta3 -f -d -s ./IOS80/00000006.app 00000006-80.delta 00000006.app
-mv 00000006.app IOS80
+./xdelta3 -f -d -s ./IOS31/00000006.app 00000006-31.delta ./IOS31/00000006.app
+./xdelta3 -f -d -s ./IOS80/00000006.app 00000006-80.delta ./IOS80/00000006.app
 printf '\e[0;31m%s\e[0m \n' '4. Packing the new IOS...'
 mkdir WAD
 mono Sharpii.exe WAD -p IOS31/ WAD/IOS31.wad -f
