42da00a73c097e81c667530b3eeb33e55e5e382c
larsenv <larsen@weborican.com>
2016-12-03 09:06:28 -0600

    Fixed how the patches are applied

--- Patcher - Wii/Mac/patch.sh
+++ Patcher - Wii/Mac/patch.sh
@@ -10,9 +10,11 @@ cp IOS31/000000010000001fv3608.wad IOS31-old.wad
 cp IOS80/0000000100000050v6944.wad IOS80-old.wad
 mono Sharpii.exe WAD -u IOS31-old.wad IOS31/
 mono Sharpii.exe WAD -u IOS80-old.wad IOS80/
-printf '\e[0;31m%s\e[0m \n' '3. Patching the APP file with the new key...'
-./xdelta3 -f -d -s ./IOS31/00000006.app 00000006-31.delta ./IOS31/00000006.app
-./xdelta3 -f -d -s ./IOS80/00000006.app 00000006-80.delta ./IOS80/00000006.app
+printf '\e[0;31m%s\e[0m \n' '3. Patching the APP file...'
+./xdelta3 -f -d -s ./IOS31/00000006.app 00000006-31.delta 00000006.app
+mv 00000006.app IOS31
+./xdelta3 -f -d -s ./IOS80/00000006.app 00000006-80.delta 00000006.app
+mv 00000006.app IOS80
 printf '\e[0;31m%s\e[0m \n' '4. Packing the new IOS...'
 mkdir WAD
 mono Sharpii.exe WAD -p IOS31/ WAD/IOS31.wad -f
