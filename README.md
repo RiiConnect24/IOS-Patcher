e4771c599553a808db20addd953c301b7327a952
Spotlight <awesomebing1@gmail.com>
2017-10-21 14:10:26 -0500

    Add UNIX vWii support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ![Production List](https://img.shields.io/discord/206934458954153984.svg?style=flat-square)
 ![Version Badge](https://img.shields.io/github/release/riiconnect24/ios-patcher.svg?style=flat-square)
 
-This patcher patches the RSA Key and applies mail patches in IOS31 and IOS80 on the Wii so you can use RiiConnect24 without errors prohibiting you.
+This patcher patches the RSA Key and applies mail patches in IOS31 for Wii/vWii and IOS80 just for Wii so you can use RiiConnect24 without errors prohibiting you.
 
 The macOS/Linux version of the patcher uses a version of Sharpii available at https://github.com/spotlightishere/sharpii
 
--- UNIX/patch.sh
+++ UNIX/patch.sh
@@ -1,44 +1,85 @@
 #!/usr/bin/env bash
 cd -P -- "$(dirname -- "$0")"
-echo "This is a patcher that creates an IOS31 and IOS80 WAD."
-echo "This is only for the Wii, please don't install this on a Wii U."
+TYPE="none"
+echo "This is a patcher that creates an IOS31 WAD and in the case of a vWii an IOS80 WAD."
 case "$OSTYPE" in
-	linux*)   
-		hash xdelta3 2>/dev/null || { echo >&2 "I'm missing xdelta3, please install with package manager, or compile and add it to your path."; exit 1; }
-		hash mono 2>/dev/null || { echo >&2 "I'm missing mono, please install with package manager, or compile and add it to your path."; exit 1; };;
-	darwin*)  
-		hash mono 2>/dev/null || { echo >&2 "I'm missing mono, please install with the homebrew package manager at https://brew.sh/";exit 1;}
-		hash xdelta3 2>/dev/null || { echo >&2 "I'm missing xdelta, please install with homebrew at https://brew.sh/"; exit 1; };;
+  linux*)
+    hash xdelta3 2>/dev/null || { echo >&2 "I'm missing xdelta3, please install with package manager, or compile and add it to your path."; exit 1; }
+    hash mono 2>/dev/null || { echo >&2 "I'm missing mono, please install with package manager, or compile and add it to your path."; exit 1; } ;;
+  darwin*)
+    hash mono 2>/dev/null || { echo >&2 "I'm missing mono, please install with the homebrew package manager at https://brew.sh/";exit 1;}
+    hash xdelta3 2>/dev/null || { echo >&2 "I'm missing xdelta, please install with homebrew at https://brew.sh/"; exit 1; } ;;
 esac
 
 if [ ! -f ./Sharpii.exe ]; then
-	echo "Warning! Missing stuff from release folder. Please extract all files."
-	echo "You can get them from https://github.com/RiiConnect24/IOS-Patcher/releases"
-	exit 1
+  echo "Warning! Missing stuff from release folder. Please extract all files."
+  echo "You can get them from https://github.com/RiiConnect24/IOS-Patcher/releases"
+  exit 1
 fi
-printf '\e[0;31m%s\e[0m \n' '1. Downloading IOS from NUS...'
-mono Sharpii.exe NUSD -ios 31 -v latest -o IOS31-old
-mono Sharpii.exe NUSD -ios 80 -v latest -o IOS80-old
-printf '\e[0;31m%s\e[0m \n' '2. Unpacking IOS...'
-cp IOS31-old/000000010000001fv3608.wad IOS31-old.wad
-cp IOS80-old/0000000100000050v6944.wad IOS80-old.wad
-mono Sharpii.exe WAD -u IOS31-old.wad IOS31/
-mono Sharpii.exe WAD -u IOS80-old.wad IOS80/
-printf '\e[0;31m%s\e[0m \n' '3. Patching the APP file...'
-xdelta3 -f -d -s ./IOS31/00000006.app ../Patches/00000006-31.delta ./IOS31/00000006.app
-xdelta3 -f -d -s ./IOS80/00000006.app ../Patches/00000006-80.delta ./IOS80/00000006.app
-printf '\e[0;31m%s\e[0m \n' '4. Packing the new IOS...'
+
+printColor () {
+  if [ -z "$@" ]; then
+    echo "Oi, please pass something for me to color."
+  else
+    printf '\e[0;31m%s\e[0m \n' "$@"
+  fi
+  return 0
+}
+
+while true; do
+  read -p "What console are you patching for? v = vWii, w = normal Wii: " vw
+  case $vw in
+    [Vv]* ) TYPE="v"; break ;;
+    [Ww]* ) TYPE="w"; break ;;
+    * ) echo "Please answer with either v or w for console." ;;
+  esac
+done
+
+if [ "$TYPE" = "v" ]; then
+	printColor '1. Looking for WAD...'
+  # Users of vWii must provide their own wad.
+  if [ ! -f ./IOS31_v3864.wad ]; then
+    echo "Warning! I'm missing the actual IOS, named IOS31_v3864.wad."
+    echo "I can't tell you how to obtain this, but please place it in the current directory under that name."
+    exit 1
+  fi
+else
+  # For Wii
+  printColor '1. Downloading IOS from NUS...'
+  mono Sharpii.exe NUSD -ios 31 -v latest -o IOS31-old
+  mono Sharpii.exe NUSD -ios 80 -v latest -o IOS80-old
+fi
+
+printColor '2. Unpacking IOS...'
+if [ "$TYPE" = "w" ]; then
+  cp IOS31-old/000000010000001fv3608.wad IOS31-old.wad
+  cp IOS80-old/0000000100000050v6944.wad IOS80-old.wad
+  mono Sharpii.exe WAD -u IOS31-old.wad IOS31/
+  mono Sharpii.exe WAD -u IOS80-old.wad IOS80/
+else
+  mono Sharpii.exe WAD -u IOS31_v3864.wad IOS31/
+fi
+printColor  '3. Patching the APP file...'
+xdelta3 -f -d -s ./IOS31/00000006.app ../Patches/"$TYPE"00000006-31.delta ./IOS31/00000006.app
+# We only do IOS80 for actual Wiis.
+if [ "$TYPE" = "w" ]; then
+  xdelta3 -f -d -s ./IOS80/00000006.app ../Patches/"$TYPE"00000006-80.delta ./IOS80/00000006.app
+fi
+printColor '4. Packing the new IOS...'
 mkdir WAD
 # This verion of Sharpii has fakesigning enabled by default.
 mono Sharpii.exe WAD -p IOS31/ ./WAD/IOS31.wad
-mono Sharpii.exe WAD -p IOS80/ ./WAD/IOS80.wad
 mono Sharpii.exe IOS ./WAD/IOS31.wad -fs -es -np -vp
-mono Sharpii.exe IOS ./WAD/IOS80.wad -fs -es -np -vp
-printf '\e[0;31m%s\e[0m \n' '5. Cleaning up files... (Type Y if needed).'
-rm IOS31-old.wad
-rm IOS80-old.wad
-rm -rf IOS31-old
-rm -rf IOS80-old
-rm -rf IOS31
-rm -rf IOS80
+
+if [ "$TYPE" = "w" ]; then
+  mono Sharpii.exe WAD -p IOS80/ ./WAD/IOS80.wad
+  mono Sharpii.exe IOS ./WAD/IOS80.wad -fs -es -np -vp
+fi
+printColor '5. Cleaning up files... (Type Y if needed).'
+# rm IOS31-old.wad
+# rm IOS80-old.wad
+# rm -rf IOS31-old
+# rm -rf IOS80-old
+# rm -rf IOS31
+# rm -rf IOS80
 echo "We're done."
